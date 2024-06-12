f9d707caa038d95919f8ee408c23f9479fe3ee90
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-12 12:46:07 +0300

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
 
-const App = () => {
-  return <h1>Hello React</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
