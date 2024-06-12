0a5392f559768ffa09545da75c0b5e5640d4ba42
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-12 13:41:06 +0300

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import Profile from "./components/Profile/Profile";
+import userData from "./components/userData";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
   </React.StrictMode>
 );
