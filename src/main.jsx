9a32336575ea74d6240fed43b71b14547da121eb
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-12 12:31:09 +0300

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+const App = () => {
+  return <h1>Hello React</h1>
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
