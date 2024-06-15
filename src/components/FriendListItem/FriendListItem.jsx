32afeb589826c8d31090744f161b374f320ae76e
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-15 15:14:26 +0300

    добавил библиотеку классикс

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,28 @@
 import css from "./FriendListItem.module.css";
 
+import clsx from "clsx"; //библиотека для написания классов если нужно что-то добавить по условию
+
 const FriendListItem = ({ image, name, status }) => {
+   const statusClass = clsx(status ? css.onLine : css.offLine);
+
    return (
       <div className={css.card}>
          <img src={image} alt="Avatar" width="48" />
          <p className={css.cardName}>{name}</p>
-         {status ? <p className={css.onLine}>OnLine</p> : <p className={css.offLine}>OffLine</p>}
+         <p className={statusClass}>{status ? "OnLine" : "OffLine"}</p>
       </div>
    );
 };
+
+// статус через тернарник
+// const FriendListItem = ({ image, name, status }) => {
+//    return (
+//       <div className={css.card}>
+//          <img src={image} alt="Avatar" width="48" />
+//          <p className={css.cardName}>{name}</p>
+//          {status ? <p className={css.onLine}>OnLine</p> : <p className={css.offLine}>OffLine</p>}
+//       </div>
+//    );
+// };
 
 export default FriendListItem;
